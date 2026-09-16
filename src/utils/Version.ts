0184b4e8a8faa421ff64d90108d00af722c896da
David Nagy <david.nagy@quinto.io>
2026-09-17 00:06:19 +0200

    3.0.3

--- src/utils/Version.ts
+++ src/utils/Version.ts
@@ -8,22 +8,23 @@ export default function getVersion(): string {
     '..',
     '..',
     '..',
-    '..',
     'package.json',
   );
-  const file = Path.join(import.meta.dirname, '..', '..', 'package.json');
-
+  const file = Path.join(import.meta.dirname, '..', 'package.json');
   let pck;
   if (fs.existsSync(parentFile)) {
     pck = fs.readFileSync(parentFile, {
       encoding: 'utf8',
     });
-  } else {
+    const json = JSON.parse(pck);
+    return json.version || '0.0.0';
+  }
+  if (file) {
     pck = fs.readFileSync(file, {
       encoding: 'utf8',
     });
+    const json = JSON.parse(pck);
+    return json.version || '0.0.0';
   }
-
-  const json = JSON.parse(pck);
-  return json.version || '0.0.0';
+  return '0.0.0';
 }
